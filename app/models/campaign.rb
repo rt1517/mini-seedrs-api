e660b4e3c0f86aa1ddd016f85fece2978caabe99
Ronald Tudu <rt151707@gmail.com>
2022-09-17 18:25:16 +0600

    feat: added pg_search

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,11 +1,21 @@
 class Campaign < ApplicationRecord
+  include PgSearch::Model
+
   has_many :investments, dependent: :destroy
 
   validates :name, :image_url, :country, :sector, :percentage_raised, :target_amount, :investment_multiple, presence: true
 
+  pg_search_scope :search_by_column, lambda { |column, query|
+    raise ArgumentError, "filter_by only allows #{attribute_names.join(',')}" unless attribute_names.map(&:to_sym).include?(column)
+    {
+      against: column,
+      query: query,
+    }
+  }
+
   def update_percentage_raised(investment)
-      investment_percentage_raised = (investment.amount / target_amount) * 100
-      total_percentage_raised = percentage_raised + investment_percentage_raised
-      update!(percentage_raised: total_percentage_raised)
+    investment_percentage_raised = (investment.amount / target_amount) * 100
+    total_percentage_raised = percentage_raised + investment_percentage_raised
+    update!(percentage_raised: total_percentage_raised)
   end
 end
